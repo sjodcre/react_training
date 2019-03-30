891df2a773bc5f512f25951ef16c02495b515a53
Samuel Omamalin <samuelomamalin@gmail.com>
2019-03-30 11:24:09 -0400

    finish redux part

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,42 +6,41 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-import {setSearchField} from '../actions';
+import {setSearchField, requestRobots} from '../actions';
 
 const mapStateToProps = state => {
 	// console.log('state', state);
 	// console.log('searchField', state.searchField);
 	return {
-		searchField: state.searchField
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error:state.requestRobots.error
 		// because only one have to do the above else is state.searchRobots.searchField
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return{
-		onSearchChange: (event) => {
-			// console.log('dispatch',dispatch(setSearchField(event.target.value)));
-			dispatch(setSearchField(event.target.value))
-		}
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+
 
 	}
 	
 }
 
 class App extends Component {
-	constructor(){
-		super()
-		this.state = {
-			robots: [],
-			// searchfield: ''
-		}
-	}
+	// constructor(){
+	// 	super()
+	// 	this.state = {
+	// 		robots: [],
+	// 		// searchfield: ''
+	// 	}
+	// }
 
 	componentDidMount(){
-		// console.log(this.props.store.getState())
-		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response =>response.json())
-		.then(users => this.setState({robots:users}));
+		this.props.onRequestRobots()
 	}
 
 	// onSearchChange = (event) => {
@@ -49,12 +48,11 @@ class App extends Component {
 	// }
 
 	render(){
-		const { robots} = this.state;
-		const { searchField, onSearchChange} = this.props;
+		const { searchField, onSearchChange, robots, isPending} = this.props;
 		const filteredRobots= robots.filter(robot =>{
 			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		});
-		return !robots.length ?
+		return isPending ?
 		<h1> Loading </h1> :
 			(
 				<div className='tc'>
